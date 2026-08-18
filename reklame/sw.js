78ab1c685543fce73f992f1077c2cd8124a22b39
Syarif99999 <syarifhamid09@gmail.com>
2026-08-18 15:27:16 +0700

    Add files via upload

---
+++ reklame/sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'wp-reklame-cache-v8';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  const isNavigation = event.request.mode === 'navigate';
+  const isAppShell = ASSETS_TO_CACHE.some(
+    (asset) => new URL(asset, self.registration.scope).href === url.href
+  );
+
+  // HTML selalu dicoba dari jaringan lebih dulu supaya perbaikan aplikasi
+  // tidak tertahan oleh salinan index.html lama di cache perangkat.
+  if (isNavigation) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  if (isAppShell) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => cached || fetch(event.request))
+    );
+  } else {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+  }
+});
